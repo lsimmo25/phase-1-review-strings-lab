4bd5c6b64a07bc754356a4bb195557390a07a203
Lyle Simmons <lylesimmons125@gmail.com>
2024-01-29 10:29:55 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Lyle";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `${welcomeMessage.slice(0,7)}, ${currentUser[0]}!`
